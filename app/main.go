4ffb8c5a8fe38dc9088d3b89a1fd57161e058bc8
zjpiazza <zjpiazza@gmail.com>
2025-04-16 08:59:21 -0500

    Read command input

--- app/main.go
+++ app/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 )
 
@@ -14,5 +15,11 @@ func main() {
 	fmt.Fprint(os.Stdout, "$ ")
 
 	// Wait for user input
-	bufio.NewReader(os.Stdin).ReadString('\n')
+	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+
+	if err != nil {
+		log.Fatal("Error reading command")
+	}
+
+	fmt.Println(command[:len(command)-1] + ": command not found")
 }
